3b0905f212be7197d7901df21750714c0de6a3d7
AndresUsugaDev <yeicbo@gmail.com>
2023-06-08 10:22:26 -0500

    Corrección a la solución del ejercicio3

--- ejerciciosLogica/ejercicio03.js
+++ ejerciciosLogica/ejercicio03.js
@@ -1,8 +1,7 @@
 /*Defino la función countNameRepetitions*/
-
 const countNameRepetitions=(nombres) => {
-    const red=[... new Set(nombres)];
     const resultado = [];
+<<<<<<< Updated upstream
     const rta =[];
     nombres.forEach(el => (resultado[el] = resultado[el] + "*" || "*"));
     //console.log(resultado)
@@ -10,7 +9,10 @@ const countNameRepetitions=(nombres) => {
         rta.push(red[i]+":"+resultado[red[i]].substring(9));
     }
     return rta;
+=======
+    nombres.forEach(el => (resultado[el] = (resultado[el]||"")+"*"));
+    return resultado
+>>>>>>> Stashed changes
     }
-
 const nombres = ["Arturo","Diego","Mateo","Juan","Diego","Andrés","Matilde","Tatiana","Andrés","Pancracio","Tatiana","Jonás","Milan","Tommy"]
 console.log(nombres,"\n","countNameRepetitions (names)",countNameRepetitions(nombres));
---
+++ ejerciciosLogica/ejercicio04.js
@@ -0,0 +1,16 @@
+/*Defino la función FizzBuzz para determinar los mpultiplos de 3 o 5*/
+const fizzBuzz=(numero) => {
+    for (let i=1; i<=numero;i++){
+        if (i%3==0 && i%5==0) {
+            console.log("FizzBuzz");
+        } else if (i%5==0) {
+            console.log("Buzz");
+        } else if (i%3==0) {
+            console.log("Fizz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+let numero = 45;
+fizzBuzz(numero);
